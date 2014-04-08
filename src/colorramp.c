d74fda250741d6103d856420cb86f603823d3ad3
Mattias Andrée <maandree@operamail.com>
2014-04-09 01:33:21 +0200

    implement LUT applying (pre and post adjustments and calibrations)
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- src/colorramp.c
+++ src/colorramp.c
@@ -21,6 +21,7 @@
 
 #include "colorramp.h"
 
+#include <string.h>
 #include <stdint.h>
 #include <math.h>
 
@@ -283,9 +284,78 @@ interpolate_color(float a, const float *c1, const float *c2, float *c)
 	c[2] = (1.0-a)*c1[2] + a*c2[2];
 }
 
+static void
+apply_lut(uint16_t *out[3], size_t out_sizes[3], gamma_ramps_t *lut)
+{
+	if (lut == NULL)
+		return;
+	size_t calib_sizes[3] = {
+		lut->red_size,
+		lut->green_size,
+		lut->blue_size
+	};
+	uint16_t *calib[3] = {
+		lut->red,
+		lut->green,
+		lut->blue
+	};
+	for (int c = 0; c < 3; c++) {
+		uint16_t *cfilter = out[c];
+		uint16_t *ccalib  = calib[c];
+		size_t gamma_size = out_sizes[c];
+		size_t size_      = calib_sizes[c] - 1;
+		for (size_t i = 0; i < gamma_size; i++) {
+			/* We a rounding a bit. We could do linear
+			   interpolation or even more advanced
+			   interpolations, but it is probably not
+			   worth it. If this is used for adjustments
+			   rather than applying the calibrations that
+			   was present when Redshift started, the
+			   lookup table can be of any size so that
+			   this issue cannot possibility be noticed.*/
+			uint16_t y = cfilter[i];
+			y = (float)y * size_ / UINT16_MAX + 0.5f;
+			y = y < 0 ? 0 : (y > size_ ? size_ : y);
+			cfilter[i] = ccalib[y];
+		}
+	}
+}
+
 void
 colorramp_fill(gamma_ramps_t out_ramps, gamma_settings_t adjustments)
 {
+	size_t gamma_sizes[3] = {
+		out_ramps.red_size,
+		out_ramps.green_size,
+		out_ramps.blue_size
+	};
+
+	uint16_t *filter[3] = {
+		out_ramps.red,
+		out_ramps.green,
+		out_ramps.blue
+	};
+
+
+	if (adjustments.lut_pre != NULL) {
+		gamma_ramps_t lut = *(adjustments.lut_pre);
+		if (lut.red_size   == out_ramps.red_size   &&
+		    lut.green_size == out_ramps.green_size &&
+		    lut.blue_size  == out_ramps.blue_size) {
+			memcpy(out_ramps.red, lut.red,
+			       (gamma_sizes[0] + gamma_sizes[1] + gamma_sizes[2]) * sizeof(uint16_t));
+		} else {
+			for (int c = 0; c < 3; c++) {
+				uint16_t *cfilter = filter[c];
+				size_t gamma_size_ = gamma_sizes[c] - 1;
+				for (size_t i = 0; i <= gamma_size_; i++)
+					cfilter[i] = (float)i / gamma_size_ * UINT16_MAX;
+			}
+			apply_lut(filter, gamma_sizes, adjustments.lut_pre);
+		}
+	}
+
+
 	/* Approximate white point */
 	int temp = (int)(adjustments.temperature + 0.5f);
 	float white_point[3];
@@ -300,13 +370,23 @@ colorramp_fill(gamma_ramps_t out_ramps, gamma_settings_t adjustments)
 		adjustments.gamma_correction[2] * adjustments.gamma
 	};
 
-#define F(Y, C)  pow((Y) * adjustments.brightness * white_point[C], \
-		     1.0f / gamma[C]) * (UINT16_MAX+1)
+#define F(Y, C)  pow((Y) * adjustments.brightness * white_point[C],	\
+			1.0f / gamma[C])
 
-	for (size_t i = 0; i < out_ramps.red_size;   i++)
-		out_ramps.red[i]   = F((float)i / out_ramps.red_size,   0);
-	for (size_t i = 0; i < out_ramps.green_size; i++)
-		out_ramps.green[i] = F((float)i / out_ramps.green_size, 1);
-	for (size_t i = 0; i < out_ramps.blue_size;  i++)
-		out_ramps.blue[i]  = F((float)i / out_ramps.blue_size,  2);
+	for (int c = 0; c < 3; c++) {
+		uint16_t *cfilter = filter[c];
+		size_t gamma_size = gamma_sizes[c];
+		for (size_t i = 0; i < gamma_size; i++)
+			cfilter[i] = F((float)i / gamma_size, c) * (UINT16_MAX+1);
+	}
+
+
+	apply_lut(filter, gamma_sizes, adjustments.lut_post);
+
+
+	/* Apply gamma ramps used when Redshift started on top of
+	   the effects of Redshift. It would be easier to put
+	   Redshift's effects on top if this, but then calibrations
+	   whould become incorrect. */
+	apply_lut(filter, gamma_sizes, adjustments.lut_calibration);
 }
