dcd949412366eaf47266662d6c97920891ef0fba
Mattias Andrée <maandree@operamail.com>
2014-04-12 07:04:19 +0200

    Add ignorable option to adjustment settings, this can be used if you want to have settings specified for monitors that are not currently plugged in as is now possible because of the EDID addition
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- src/gamma-common.h
+++ src/gamma-common.h
@@ -131,6 +131,8 @@ struct gamma_selection_state_t {
 	   duplicated. */
 	void *data;
 	size_t sizeof_data;
+	/* Whether the selection can be ignored if it fails. */
+	int ignorable;
 	/* The CRTC and partition (e.g. screen) indices. */
 	ssize_t crtc;
 	ssize_t partition;
